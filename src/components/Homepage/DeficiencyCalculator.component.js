9ece03eb9695531084652a65bde7230ed8cf18e3
Katelyn Helms <krhelms11@gatech.edu>
2020-04-06 00:41:39 -0400

    update Deficiency Symptoms

--- src/components/Homepage/DeficiencyCalculator.component.js
+++ src/components/Homepage/DeficiencyCalculator.component.js
@@ -1,41 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const proteinSymptoms = ["Edema", "Mood Changes", "Weakness/Fatigue", "Fatty Liver", "Flaky, splitting, or red skin", "Hair Issues(thinning, faded color, hair loss)", "Brittle Nails", "Loss of muscle mass", "Weakened bones", "Stunted growth", "Weakened immune system", "Increased appetite"];
+const proteinSymptoms = ["Edema", "Mood Changes", "Weakness/Fatigue", "Fatty Liver", "Flaky, Splitting, or Red skin", "Hair Issues(thinning, faded color, hair loss)", "Brittle Nails", "Loss of Muscle Mass", "Weakened Bones", "Stunted Growth", "Weakened Immune System", "Increased Appetite"];
 const proteinList = proteinSymptoms.map((proteinSymptoms) =>
     <li>{proteinSymptoms}</li>
 );
 
-const carbSymptoms = ["Lack of energy", "Constipation", "Nausea", "Dizziness", "Headaches", "Bad Breath", "Sugar Cravings", "Loss of appetite"];
+const carbSymptoms = ["Lack of Energy", "Constipation", "Nausea", "Dizziness", "Headaches", "Bad Breath", "Sugar Cravings", "Loss of Appetite"];
 const carbsList = carbSymptoms.map((carbSymptoms) =>
     <li>{carbSymptoms}</li>
 );
 
-const fatsSymptoms = ["Dry/Scaly skin", "Dry Eyes", "Poor Body Temperature Regulation", "Dry Hair", "Hair Loss", "Hormonal Problems", "Increased Hunger", "Mental Fatigue", "Fatigue"];
+const fatsSymptoms = ["Dry, Scaly skin", "Dry Eyes", "Poor Body Temperature Regulation", "Dry Hair", "Hair Loss", "Hormonal Problems", "Increased Hunger", "Mental Fatigue", "Fatigue"];
 const fatsList = fatsSymptoms.map((fatsSymptoms) =>
     <li>{fatsSymptoms}</li>
 );
 
-const sodiumSymptoms = ["Nausea", "Vomiting", "Confusion", "Drowsiness", "Irritability/Restlessness", "Muscle weakness, spasms, or cramps", "Seizures", "Coma"];
+const sodiumSymptoms = ["Nausea", "Vomiting", "Confusion", "Drowsiness", "Irritability or Restlessness", "Muscle Weakness, Spasms, or Cramps", "Seizures", "Coma"];
 const sodiumList = sodiumSymptoms.map((sodiumSymptoms) =>
     <li>{sodiumSymptoms}</li>
 );
 
-const calciumSymptoms = ["Muscle problems", "Fatigue", "Dry, Itchy Skin", "Dry, Brittle Nails", "Osteoporosis", "Osteopenia", "Depression", "Tooth Decay"];
+const calciumSymptoms = ["Muscle Problems", "Fatigue", "Dry, Itchy Skin", "Dry, Brittle Nails", "Osteoporosis", "Osteopenia", "Depression", "Tooth Decay"];
 const calciumList = calciumSymptoms.map((calciumSymptoms) =>
     <li>{calciumSymptoms}</li>
 );
 
-const ironSymptoms = ["Tiredness", "Paleness", "Shortness of Breath", "Headaches", "Dizziness", "Dry or Damaged Hair and Skin", "Heart Palpitations", "Swelling/Soreness of Tongue and Mouth", "Restless Legs", "Brittle Nails", "Strange Cravings (ice, clay, dirt, chalk, paper)", "Anxiety", "Cold hands and feet", "Frequent Infections"];
+const ironSymptoms = ["Tiredness", "Paleness", "Shortness of Breath", "Headaches", "Dizziness", "Dry or Damaged Hair and Skin", "Heart Palpitations", "Swelling or Soreness of Tongue and Mouth", "Restless Legs", "Brittle Nails", "Strange Cravings (ice, clay, dirt, chalk, paper)", "Anxiety", "Cold Hands and Feet", "Frequent Infections"];
 const ironList = ironSymptoms.map((ironSymptoms) =>
     <li>{ironSymptoms}</li>
 );
 
-const vitCSymptoms = ["Dry skin", "Splitting Hair", "Swelling and discoloration of gums", "Sudden and unexpected bleeding from gums", "Nosebleeds", "Poor healing of wounds", "Slow healing of wounds", "Tooth loss"];
+const vitCSymptoms = ["Dry Skin", "Splitting Hair", "Swelling and Discoloration of Gums", "Sudden and Unexpected Bleeding from Gums", "Nosebleeds", "Poor Wound Healing", "Tooth Loss"];
 const vitCList = vitCSymptoms.map((vitCSymptoms) =>
     <li>{vitCSymptoms}</li>
 );
 
+const vitASymptoms = ["Dry Skin", "Dry Eyes", "Night Blindness", "Infertility", "Delayed Growth", "Throat or Chest Infections", "Poor Wound Healing", "Acne or Breakouts"];
+const vitAList = vitASymptoms.map((vitASymptoms) =>
+<li>{vitASymptoms}</li>
+);
+
+const vitESymptoms = ["Muscle Weakness", "Bad Coordination", "Numbness", "Vision Deterioration", "Weak Immune System"];
+const vitEList = vitESymptoms.map((vitESymptoms) =>
+<li>{vitESymptoms}</li>
+);
+
+const potassiumSymptoms = ["Weakness", "Fatigue", "Muscle Cramps or Spasms", "Digestive Problems", "Heart Palpitations", "Muscle Aches or Stiffness", "Tingling or Numbness", "Breathing Difficulties", "Mood Changes"];
+const potassiumList = potassiumSymptoms.map((potassiumSymptoms) =>
+<li>{potassiumSymptoms}</li>
+);
+
+const magnesiumSymptoms = ["Nausea", "Vomiting", "Loss of Appetite", "Muscle Twitches or Cramps", "Mental Disorders", "Osteoporosis", "Fatigue", "Muscle Weakness", "High Blood Pressure", "Asthma", "Irregular Heartbeat"];
+const magnesiumList = magnesiumSymptoms.map((magnesiumSymptoms) =>
+<li>{magnesiumSymptoms}</li>
+);
+
+const fiberSymptoms = ["Stomach Issues", "Constipation", "Weight Gain", "Blood Sugar Fluctuations", "Hunger after Eating", "High Cholesterol", "High Blood Pressure", "Inflammation", "Low Energy", "Nausea"];
+const fiberList = fiberSymptoms.map((fiberSymptoms) =>
+<li>{fiberSymptoms}</li>
+);
 
 const DeficiencyCalculator = (property) => {
     const [foods, setFoods] = useState([]);
@@ -118,6 +142,46 @@ const DeficiencyCalculator = (property) => {
                 <div>
                     <h3>Vitamin C Deficiency Symptoms</h3>
                     <ul>{vitCList}</ul>
+                </div>
+                }  
+            </div>
+            <div className="defContainer">
+                { foods.vitaminA / user.vitaminA < 0.5 &&
+                <div>
+                    <h3>Vitamin A Deficiency Symptoms</h3>
+                    <ul>{vitAList}</ul>
+                </div>
+                }  
+            </div>
+            <div className="defContainer">
+                { foods.vitaminE / user.vitaminE < 0.5 &&
+                <div>
+                    <h3>Vitamin E Deficiency Symptoms</h3>
+                    <ul>{vitEList}</ul>
+                </div>
+                }  
+            </div>
+            <div className="defContainer">
+                { foods.potassium / user.potassium < 0.5 &&
+                <div>
+                    <h3>Potassium Deficiency Symptoms</h3>
+                    <ul>{potassiumList}</ul>
+                </div>
+                }  
+            </div>
+            <div className="defContainer">
+                { foods.magnesium / user.magnesium < 0.5 &&
+                <div>
+                    <h3>Magnesium Deficiency Symptoms</h3>
+                    <ul>{magnesiumList}</ul>
+                </div>
+                }  
+            </div>
+            <div className="defContainer">
+                { foods.fiber / user.fiber < 0.5 &&
+                <div>
+                    <h3>Fiber Deficiency Symptoms</h3>
+                    <ul>{fiberList}</ul>
                 </div>
                 }  
             </div>
