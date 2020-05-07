d7650b054588a92f55698f11c72ed1d2f6830405
Woodson, Mark Stephen <mwoodson11@gatech.edu>
2020-05-07 10:40:13 -0700

    Updates

---
+++ precache-manifest.5547966a26c12796468a67a8ac7b500c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5227fb34d742c1c870557c7b65438d",
+    "url": "/nutrifficient/index.html"
+  },
+  {
+    "revision": "0378e311408c38ce46eb",
+    "url": "/nutrifficient/static/css/2.bc9c845d.chunk.css"
+  },
+  {
+    "revision": "0d7f065cacfe7c0c07d4",
+    "url": "/nutrifficient/static/css/main.17899951.chunk.css"
+  },
+  {
+    "revision": "0378e311408c38ce46eb",
+    "url": "/nutrifficient/static/js/2.27851560.chunk.js"
+  },
+  {
+    "revision": "80c84b08d9743cc6ae8c6b1c72cc9718",
+    "url": "/nutrifficient/static/js/2.27851560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7f065cacfe7c0c07d4",
+    "url": "/nutrifficient/static/js/main.a81067bb.chunk.js"
+  },
+  {
+    "revision": "88d9aaac4337ad3dbbb1",
+    "url": "/nutrifficient/static/js/runtime-main.b8d76d38.js"
+  }
+]);
